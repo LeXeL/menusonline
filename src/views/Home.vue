774b153e86f73e599b5bc2e89e65755798abc0d2
Diego Rodriguez <lexelEZ@gmail.com>
2020-06-04 22:40:38 -0500

    home vue updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,43 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="../assets/logo.png">
-  </q-page>
+    <div class="home">
+        <input type="text" placeholder="Name" v-model="name" />
+        <br />
+        <input type="text" placeholder="Url" v-model="url" />
+        <br />
+        <button @click="Generate()">Generar</button>
+        <div id="qrcode"></div>
+        <div v-for="(element, index) in queue" :key="index">
+            <pdfembeded :url="element.url"></pdfembeded>
+        </div>
+    </div>
 </template>
 
-<style>
-</style>
-
 <script>
+// @ is an alias to /src
+import pdfembeded from '@/components/pdfembeded.vue'
+import QRCode from 'easyqrcodejs'
+
 export default {
-  name: 'PageHome'
+    name: 'Home',
+    components: {
+        pdfembeded,
+    },
+    data() {
+        return {
+            queue: [],
+            url: 'http://demplays.com/Beginningchords.pdf',
+            name: 'chrods',
+        }
+    },
+    methods: {
+        Generate() {
+            var options = {
+                text: this.url,
+            }
+            var qrcode = new QRCode(document.getElementById('qrcode'), options)
+
+            this.queue.push({name: this.name, url: this.url})
+        },
+    },
 }
 </script>
