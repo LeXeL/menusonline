c72432454e89faff6aa7428b86ff4bcfba9bd5c7
Diego Rodriguez <lexelEZ@gmail.com>
2020-06-04 23:41:10 -0500

    ya no me acuerdo que hice aqui

--- src/main.js
+++ src/main.js
@@ -3,9 +3,24 @@ import App from './App.vue'
 import router from './router'
 import './quasar'
 
+import firebase from 'firebase/app'
+
 Vue.config.productionTip = false
 
+const firebaseConfig = {
+    apiKey: 'AIzaSyBQdeyP1g8ciatnKYMd_ZzdPzF-Jm8Alvk',
+    authDomain: 'menusonline-f988f.firebaseapp.com',
+    databaseURL: 'https://menusonline-f988f.firebaseio.com',
+    projectId: 'menusonline-f988f',
+    storageBucket: 'menusonline-f988f.appspot.com',
+    messagingSenderId: '714514385228',
+    appId: '1:714514385228:web:74371a3f4137cb3ceba7e9',
+    measurementId: 'G-KVTDW7X2PD',
+}
+
+firebase.initializeApp(firebaseConfig)
+
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App),
 }).$mount('#app')
--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,10 +1,19 @@
 <template>
-    <div></div>
+    <div id="menu"></div>
 </template>
 
 <script>
-export default {}
+import PDFObject from 'pdfobject'
+export default {
+    mounted() {
+        PDFObject.embed(this.url, '#menu')
+    },
+}
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.pdfobject-container {
+    height: 30rem;
+    border: 1rem solid rgba(0, 0, 0, 0.1);
+}
 </style>
