d46e9504ab3ea8110e5879ef3b5f53fb52e78dfb
Diego Rodriguez <lexelEZ@gmail.com>
2020-07-13 16:48:23 -0500

    implemented function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,13 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+})
+
+const cors = require('cors')({
+    origin: true,
+})
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +15,27 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.returnRestaurantsNames = functions.https.onRequest(async (req, res) => {
+    cors(req, res, async () => {
+        let data = []
+        try {
+            const db = admin.firestore()
+            db.collection('Restaurantes')
+                .get()
+                .then(snapshot => {
+                    snapshot.forEach(doc => {
+                        data.push(doc.data().restaurantName)
+                    })
+                })
+                .catch(err => {
+                    console.log('Error getting documents', err)
+                })
+
+            res.status(200).send({status: data})
+        } catch (err) {
+            console.log(err)
+            res.status(400).send({err: err})
+        }
+    })
+})
