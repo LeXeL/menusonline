b74aabe11ff3b9b04750e17202b75c21ec482357
Diego Rodriguez <lexelEZ@gmail.com>
2020-07-28 19:36:13 -0500

    implemented aos

--- src/main.js
+++ src/main.js
@@ -11,6 +11,10 @@ import BrewthersAlert from '@/components/general/Alert'
 import Confirm from '@/components/general/Confirm'
 import VueSmoothScroll from 'vue2-smooth-scroll'
 
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+AOS.init()
+
 Vue.use(VueSmoothScroll)
 
 Vue.config.productionTip = false
