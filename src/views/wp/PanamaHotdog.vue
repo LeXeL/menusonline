127a3a314e234d26c94c6913676ee39308617739
KidDarknezz <darkslave@live.com>
2020-09-08 21:12:27 -0500

    Message fix

--- src/views/wp/PanamaHotdog.vue
+++ src/views/wp/PanamaHotdog.vue
@@ -965,7 +965,7 @@ export default {
             let message =
                 'Buenas, me gustaria realizar un pedido de:%0D%0A%0D%0A'
             for (let item of this.cart) {
-                message += `- (${item.amount}) ${item.title} con ${item.options.title}%0D%0A`
+                message += `- (${item.amount}) ${item.title} - ${item.options.title}%0D%0A`
             }
             message += `%0D%0ANo. de pedido: ${this.orderNo}%0D%0ANombre: ${this.name}`
             if (this.specialComments.length > 0)
