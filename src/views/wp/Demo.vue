84c05bfb596a11646c088adec8228ea47e578628
Diego Rodriguez <lexelEZ@gmail.com>
2020-10-04 12:14:01 -0500

    implemented google sheet on demo

--- src/views/wp/Demo.vue
+++ src/views/wp/Demo.vue
@@ -958,7 +958,7 @@ export default {
                 data.direcion_2 = this.address
             }
             var url =
-                'https://script.google.com/macros/s/AKfycbzPCB7GpZlqm0iKBy8mDPpa12_QFmcTukrsPlaINRFSYjWGvfD9/exec'
+                'https://script.google.com/macros/s/AKfycbybmCSxZchLRwk4V4B3ev_D0mIyXPiDtXTEA0lrBmgcGAetIJo/exec'
             var xhr = new XMLHttpRequest()
             xhr.open('POST', url)
             // xhr.withCredentials = true;
@@ -1052,7 +1052,7 @@ export default {
                 this.$analytics.logEvent('wp-demo', {
                     content_action: 'Orden Completada',
                 })
-                // await this.sendToGoogleDriveSheet()
+                await this.sendToGoogleDriveSheet()
                 window.location.href = `https://wa.me/507${
                     this.whatsappNumber
                 }?text=${this.generateMessage()}`
