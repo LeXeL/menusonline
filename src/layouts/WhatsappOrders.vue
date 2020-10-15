ff66893954b5f49f78585237567ce044e7744580
Alejandro Millan <darkslave@live.com>
2020-10-15 10:58:20 -0500

    menu fix to desktop

--- src/layouts/WhatsappOrders.vue
+++ src/layouts/WhatsappOrders.vue
@@ -1,23 +1,29 @@
 <template>
     <q-layout view="hHh lpR lFr">
-        <q-page-container>
-            <router-view />
-            <footer :style="displayFooter ? 'margin-bottom:67px':''">
-                <div class="container">
-                    <div class="row text-center">
-                        <div class="col">
-                            <a href="https://www.instagram.com/blueballoon.io/">
-                                <q-img
-                                    :src="require('@/assets/landing/logo_grey.png')"
-                                    style="width: 150px;"
-                                />
-                            </a>
+        <q-page-container class="bg-grey-4">
+            <div class="row">
+                <div class="col"></div>
+                <div class="col-lg-2 col-md-3 col-sm-4 shadow-10">
+                    <router-view />
+                            <footer :style="displayFooter ? 'margin-bottom:67px':''">
+                        <div class="container">
+                            <div class="row text-center">
+                                <div class="col">
+                                    <a href="https://www.instagram.com/blueballoon.io/">
+                                        <q-img
+                                            :src="require('@/assets/landing/logo_grey.png')"
+                                            style="width: 150px;"
+                                        />
+                                    </a>
 
-                            <p>Powered By a BlueBalloon Inc.</p>
+                                    <p>Powered By a BlueBalloon Inc.</p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    </footer>
                 </div>
-            </footer>
+                <div class="col"></div>
+            </div>
         </q-page-container>
     </q-layout>
 </template>
