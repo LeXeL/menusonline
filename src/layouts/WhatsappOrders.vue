e9bbd7624434bb9e5648178c1ebb061c1f43d68c
Alejandro Millan <darkslave@live.com>
2020-12-01 16:15:37 -0500

    layout fix

--- src/layouts/WhatsappOrders.vue
+++ src/layouts/WhatsappOrders.vue
@@ -3,7 +3,7 @@
         <q-page-container class="bg-grey-4">
             <div class="row">
                 <div class="col desktop-only"></div>
-                <div class="col-lg-3 col-md-3 col-sm-4 shadow-10 full-width">
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 shadow-10">
                     <router-view />
                     <footer :style="displayFooter ? 'margin-bottom:67px' : ''">
                         <div class="container">
